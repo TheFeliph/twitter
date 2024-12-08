a8f2429f5f5d8a75b2727b918631c81c949e3f1c
Felipe Nasciemnto <Feliph08@hotmail.com>
2024-12-08 12:53:41 -0300

    Initial commit

--- backend/api/hello.js
+++ backend/api/hello.js
@@ -1,4 +1,4 @@
-// backend/api/hello.js
-module.exports = (req, res) => {
-    res.status(200).json({ message: 'Hello from the backend!' });
-};
+export default function handler(req, res) {
+    res.status(200).json({ message: "Hello from Vercel!" });
+  }
+  
